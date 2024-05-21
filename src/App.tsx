49df42becadfc66a4be903533bcdd3ffd61a07a1
Rakhmanin Aleksandr <a.rachmanin@yandex.ru>
2024-05-21 11:56:13 +0600

    Изменил и залил

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from 'react';
+import CardView from "./СardView/СardView";
+import CardDeck from "./lib/CardDeck";
+import Card from "./lib/Card";
+import PokerHand from "./lib/PokerHand";
+import './cards.css';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
+    const [cards, setCards] = useState<Card []>([]);
 
-export default App
+    const pokerHand = new PokerHand(cards);
+    const pokerHandText:string = pokerHand.getOutcome();
+
+    const getCards = () => {
+        const cardDeck = new CardDeck();
+        const newCards = cardDeck.getCards(5);
+        setCards(newCards);
+    }
+
+    if (cards.length === 0) {
+        return (
+            <div>
+                <button className='start-btn' onClick={getCards}>Dealing cards</button>
+                <p className='title-state'>No Cards</p>
+            </div>
+        );
+    }
+
+    return (
+        <div className="App">
+            <button className='start-btn' onClick={getCards}>Dealing cards</button>
+            <div className="cards">
+                {cards.map((card) => (
+                    <CardView key={card.rank + card.suit} rank={card.rank} suit={card.suit}/>
+                ))}
+            </div>
+            <h1>Poker Hand :</h1>
+            <p><b>{pokerHandText}</b></p>
+
+        </div>
+    );
+};
+
+export default App;
